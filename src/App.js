5ff2a8a4dfd67ac68f5c89e641a46c9fd6e5941a
Kamto <kam_to@outlook.com>
2024-02-20 14:47:38 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
       <div className="card">
         <div className="header">
           <div className="logo">
-            <a href=".">Kam-To</a>
+            <a href=".">Kamto Rocks</a>
           </div>
           <div className="social">
             <a href="https://facebook.com" title="Facebook" target="_blank" rel="noopener noreferrer">
@@ -34,7 +34,7 @@ class App extends React.Component {
           </a>
         </div>
         <div className="footer">
-          <span>made by <a className="underlined" href="https://github.com" target="_blank" rel="noopener noreferrer">Kam-To</a> using <a className="underlined" href="https://reactjs.org/" title="ReactJS" target="_blank" rel="noopener noreferrer">React</a> | <a className="underlined" href="https://github.com" title="GitHub repo" target="_blank" rel="noopener noreferrer">GitHub</a></span>
+          <span>made by <a className="underlined" href="https://github.com" target="_blank" rel="noopener noreferrer">Kamto Rocks</a> using <a className="underlined" href="https://reactjs.org/" title="ReactJS" target="_blank" rel="noopener noreferrer">React</a> | <a className="underlined" href="https://github.com" title="GitHub repo" target="_blank" rel="noopener noreferrer">GitHub</a></span>
         </div>
       </div>
     );
